8606e99528c1f02fa0eabdf43fe9dee4f4176147
tetsucabra <35786264+tetsucabra@users.noreply.github.com>
2020-12-08 14:08:03 -0500

    Add files via upload

---
+++ polybar/autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/fish
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; end
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+polybar bar1 &
+#polybar bar2 &
+#polybar bar3 &
+#polybar bar4 &
+
+echo "Polybar launched..."
